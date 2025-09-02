bfe4c4cd6b2495f5b8f39e88be1701d6f4757bca
q.chan <q.chan@digipen.edu>
2025-09-02 23:52:51 +0800

    implement css

--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,17 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+      <View className="flex-1 justify-center items-center">
       <Text>Edit app/index.tsx to edit this screen.</Text>
-        <Text>Hello world!</Text>
+          {/*the text-primary is form extend in tailwind.config.js or u can just put text-blue-500*/}
+        <Text className="text-5xl text-accent font-bold">Hello world! no hi</Text>
     </View>
   );
 }
